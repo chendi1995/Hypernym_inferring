b836a6abb274dad197664c4c620e00e431c44b2f
shengyp <shengyp2011@163.com>
2017-12-27 15:25:34 +1100

    first commit

---
+++ TextSimi.py
@@ -0,0 +1,37 @@
+'''
+@author:shengyp
+@contact:shengyp2011@163.com
+@file:__init__.py.py
+@time:2017/12/7 17:16
+'''
+import sys
+import numpy as np
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+
+def fun():
+    s = "你好"
+    system_coding = sys.getdefaultencoding()
+
+
+def tf_idfSimilarity():
+    pass
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    # fun()
+
+    pass
+
+
+
+
